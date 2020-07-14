66212ace3264de4bfb8d87c67faf3def87935e80
Cyrus <csarkosh@uw.edu>
2020-07-13 21:05:11 -0700

    Cache static assets attempt 1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -65,24 +65,40 @@ resource "aws_cloudfront_distribution" "cdn" {
   aliases = ["${local.domain_name}"]
   enabled = true
   default_root_object = "index.html"
-  "origin" {
+  origin {
     domain_name = "${aws_s3_bucket.webbucket.bucket_regional_domain_name}"
     origin_id = "${aws_s3_bucket.webbucket.id}"
   }
-  "restrictions" {
-    "geo_restriction" {
+  restrictions {
+    geo_restriction {
       restriction_type = "none"
     }
   }
-  "viewer_certificate" {
+  viewer_certificate {
     acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
     ssl_support_method = "sni-only"
   }
   ordered_cache_behavior {
     allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
-    "forwarded_values" {
-      "cookies" {
+    forwarded_values {
+      cookies {
+        forward = "none"
+      }
+      query_string = false
+    }
+    min_ttl = 31536000
+    default_ttl = 31536000
+    max_ttl = 31536000
+    path_pattern = "/static/*"
+    target_origin_id = "${aws_s3_bucket.webbucket.id}"
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  ordered_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    forwarded_values {
+      cookies {
         forward = "none"
       }
       query_string = false
@@ -94,11 +110,11 @@ resource "aws_cloudfront_distribution" "cdn" {
     target_origin_id = "${aws_s3_bucket.webbucket.id}"
     viewer_protocol_policy = "redirect-to-https"
   }
-  "default_cache_behavior" {
+  default_cache_behavior {
     allowed_methods = ["HEAD", "GET"]
     cached_methods = ["HEAD", "GET"]
-    "forwarded_values" {
-      "cookies" {
+    forwarded_values {
+      cookies {
         forward = "none"
       }
       query_string = false
