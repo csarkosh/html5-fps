8b177e21c0d3ac91d9343cc76c245aee43e6faca
Cyrus <csarkosh@uw.edu>
2020-07-13 20:08:16 -0700

    Port to Babylonjs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {withStyles} from "@material-ui/core"
-import { FaGithub as Github } from 'react-icons/fa'
-import { AppBar, Button, IconButton, Tooltip } from '@material-ui/core'
-import triColorBlendTriangle from './webgl/tri-color-blend-triangle'
-import twoTriangles from './webgl/two-triangles'
-import manyRectangles from './webgl/many-rectangles'
+import { AppBar, Button, IconButton, Menu, MenuItem } from '@material-ui/core'
+import { Launch, Fullscreen, MoreVert } from '@material-ui/icons'
+import * as B from '@babylonjs/core'
+import * as M from '@babylonjs/materials'
 import ProgramButton from "./ProgramButton";
+import CanvasSettingsButton from "./CanvasSettingsButton";
+import VideoSettings from "./VideoSettings";
 
+const APP_BAR_HEIGHT = '48px'
 
 const styles = theme => ({
     appBarRoot: {
+        height: APP_BAR_HEIGHT,
         padding: '6px 8px',
         '& > div': {
             display: 'flex',
@@ -46,63 +49,73 @@ const styles = theme => ({
     canvas: {
         maxWidth: 640,
         maxHeight: 480,
-        width: '100%'
+        width: '100%',
+        '&:focus': {
+            outline: 'none',
+        }
     },
-    ghButtonRoot: {
+    settingsButton: {
         color: 'white',
         marginRight: 8,
-        padding: 4,
+        padding: 6,
         '& svg': {
             fontSize: 28,
         }
     },
+    buttonWrapper: {
+        marginTop: 8,
+        '& > button + button': {
+            marginLeft: 24,
+        },
+    },
 })
 
-const programs = [
-    {
-        name: '50 Rectangles',
-        program: manyRectangles,
-    },
-    {
-        name: 'Tri-Color Blend Triangle',
-        program: triColorBlendTriangle,
-    },
-    {
-        name: 'Two Triangles',
-        program: twoTriangles
-    }
-]
 
 class App extends React.Component {
     /** @type {React.RefObject} */
     canvas = React.createRef()
-    /** @type {WebGLRenderingContext} */
-    gl = undefined
-
-    currProgram = undefined
+    /** @type {Engine} */
+    engine = null
 
     state = {
-        active: undefined
+        settingsAnchorEl: null,
+        res: '640x480',
+        resAnchorEl: null,
     }
 
     componentDidMount() {
-        const canvas = this.canvas
-        this.gl = canvas.current.getContext('webgl')
-        this.startProgram(programs[0].name, programs[0].program)()
+        this.engine = new B.Engine(this.canvas.current)
+        const scene = new B.Scene(this.engine)
+        const camera = new B.FreeCamera('camera1', new B.Vector3(0, 5, -10), scene)
+        camera.setTarget(new B.Vector3.Zero())
+        camera.attachControl(this.canvas.current, true)
+        const light = new B.HemisphericLight('light1', new B.Vector3(0, 1, 0), scene)
+        light.intensity = 0.7
+        const material = new M.GridMaterial('grid', scene)
+        const sphere = B.Mesh.CreateSphere('sphere1', 16, 2, scene)
+        sphere.position.y = 2
+        sphere.material = material
+        this.engine.runRenderLoop(() => scene.render())
     }
 
-    startProgram = (key, program) => () => {
-        if (key === this.state.active) return;
-        if (this.currProgram) {
-            this.currProgram.end()
-        }
-        this.setState({ active: key })
-        this.currProgram = program
-        this.currProgram.start(this.gl)
+    enterFullscreen = () => {
+        if (!this.engine) return;
+        this.engine.enterFullscreen()
     }
+
+    resClose = () => this.setState({ resAnchorEl: null })
+
+    resOpen = e => this.setState({ resAnchorEl: e.currentTarget })
+
+    setRes = e => this.setState({ res: e.target.value, resAnchorEl: null })
+
+    settingsClose = () => this.setState({ settingsAnchorEl: null })
+
+    settingsOpen = e => this.setState({ settingsAnchorEl: e.currentTarget })
 
     render() {
         const { classes } = this.props
+        const [width, height] = this.state.res.split('x')
         return (
             <React.Fragment>
                 <AppBar
@@ -121,26 +134,36 @@ class App extends React.Component {
                         >
                             webgl.csarko.sh
                         </Button>
-                        <Tooltip title="Link to source code">
-                            <IconButton
-                                aria-label="Link to source code"
-                                classes={{ root: classes.ghButtonRoot }}
+                        <IconButton
+                            aria-label="Link to source code"
+                            classes={{ root: classes.settingsButton }}
+                            onClick={this.settingsOpen}
+                        >
+                            <MoreVert />
+                        </IconButton>
+                        <Menu
+                            anchorEl={this.state.settingsAnchorEl}
+                            keepMounted
+                            onClose={this.settingsClose}
+                            open={Boolean(this.state.settingsAnchorEl)}
+                        >
+                            <MenuItem
                                 component="a"
                                 href="https://github.com/csarkosh/webgl-react"
                                 rel="noopener"
                                 target="_blank"
                             >
-                                <Github />
-                            </IconButton>
-                        </Tooltip>
+                                Source Code <Launch />
+                            </MenuItem>
+                        </Menu>
                     </div>
                 </AppBar>
                 <div className={classes.body}>
                     <div>
-                        <div>
+                        <div className={classes.canvasWrapper}>
                             <canvas
-                                height={480}
-                                width={640}
+                                height={height}
+                                width={width}
                                 className={classes.canvas}
                                 ref={this.canvas}
                                 id="glcanvas"
@@ -148,8 +171,32 @@ class App extends React.Component {
                             </canvas>
                         </div>
                     </div>
+                    <div className={classes.buttonWrapper}>
+                        <CanvasSettingsButton
+                            icon={<Fullscreen color="inherit" fontSize="large" />}
+                            label="Fullscreen"
+                            onClick={this.enterFullscreen}
+                        />
+                        <CanvasSettingsButton
+                            icon={<VideoSettings color="inherit" fontSize="large" />}
+                            label="Resolution"
+                            onClick={this.resOpen}
+                        />
+                        <Menu
+                            anchorEl={this.state.resAnchorEl}
+                            keepMounted
+                            onClose={this.resClose}
+                            open={Boolean(this.state.resAnchorEl)}
+                        >
+                            <MenuItem component="button" onClick={this.setRes} value="640x480">480p</MenuItem>
+                            <MenuItem component="button" onClick={this.setRes} value="1280x720">720p</MenuItem>
+                            <MenuItem component="button" onClick={this.setRes} value="1920x1080">1080p</MenuItem>
+                            <MenuItem component="button" onClick={this.setRes} value="2560x1440">1440p</MenuItem>
+                            <MenuItem component="button" onClick={this.setRes} value="3840x2160">2160p</MenuItem>
+                        </Menu>
+                    </div>
                     <div>
-                        {programs.map(({ name, program }) => (
+                        {[].map(({ name, program }) => (
                             <ProgramButton
                                 active={this.state.active === name}
                                 key={name}
