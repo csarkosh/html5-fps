58fa0c59b3bea26689506cd21ad56d56a22caf5f
Cyrus <csarkosh@uw.edu>
2020-08-06 22:51:54 -0700

    Convert spectre.css overrides to sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
 import 'spectre.css';
-import './spectre.overrides.css'
+import './spectre.overrides.scss'
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
