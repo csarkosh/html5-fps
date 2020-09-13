e4e4265b1e8903668a89e752c130d75b4d85a84a
Cyrus <csarkosh@uw.edu>
2020-09-12 21:41:16 -0700

    Freeze world matrix for static meshes

--- src/game/RoomFactory.js
+++ src/game/RoomFactory.js
@@ -25,43 +25,6 @@ export default class RoomFactory {
         const root = new TransformNode('root', this.#scene)
         const pbr2 = this.#matFactory.create('Metal_Plate_41', { uScale: 2 * WALL_WIDTH / WALL_HEIGHT, vScale: 2 })
 
-        // Create walls
-        const wall1 = MeshBuilder.CreatePlane('wall1', { height: WALL_HEIGHT, width: WALL_WIDTH })
-        wall1.material = pbr2
-        wall1.parent = root
-        wall1.position = new Vector3(0, WALL_HEIGHT / 2, 50)
-        wall1.rotation = Vector3.Zero()
-        const wall2 = wall1.createInstance('wall2')
-        wall2.parent = root
-        wall2.position.x = 50
-        wall2.position.z = 0
-        wall2.rotation.y = Math.PI / 2
-        const wall3 = wall1.createInstance('wall3')
-        wall3.parent = root
-        wall3.position.x = 0
-        wall3.position.z = -50
-        wall3.rotation.y = Math.PI
-        const wall4 = wall1.createInstance('wall4')
-        wall4.parent = root
-        wall4.position.x = -50
-        wall4.position.z = 0
-        wall4.rotation.y = 3 * Math.PI / 2
-
-        // Create ground
-        const ground1 = MeshBuilder.CreateGround('ground1', { height: 50, width: 50, subdivisions: 1 })
-        ground1.material = this.#matFactory.create('Metal_Plate_15', {pScale: 0.01, uScale: 20, vScale: 20})
-        ground1.parent = root
-        ground1.position = new Vector3(-25, 0, -25)
-        const ground2 = ground1.createInstance('ground2')
-        ground2.parent = root
-        ground2.position = new Vector3(-25, 0, 25)
-        const ground3 = ground1.createInstance('ground3')
-        ground3.parent = root
-        ground3.position = new Vector3(25, 0, -25)
-        const ground4 = ground1.createInstance('ground4')
-        ground4.parent = root
-        ground4.position = new Vector3(25, 0, 25)
-
         // Create lighting
         const hemLight = new HemisphericLight('hemLight', Vector3.Up(), this.#scene)
         hemLight.groundColor = new Color3(36 / 255, 40 / 255, 60 / 255)
@@ -71,6 +34,51 @@ export default class RoomFactory {
         this.#createPointLight(new Vector3(-25, 10, -25), root)
         this.#createPointLight(new Vector3(-25, 10, 25), root)
         this.#createPointLight(new Vector3(25, 10, 0), root)
+
+        // Create walls
+        const wall1 = MeshBuilder.CreatePlane('wall1', { height: WALL_HEIGHT, width: WALL_WIDTH })
+        wall1.material = pbr2
+        wall1.parent = root
+        wall1.position = new Vector3(0, WALL_HEIGHT / 2, 50)
+        wall1.rotation = Vector3.Zero()
+        wall1.freezeWorldMatrix()
+        const wall2 = wall1.createInstance('wall2')
+        wall2.parent = root
+        wall2.position.x = 50
+        wall2.position.z = 0
+        wall2.rotation.y = Math.PI / 2
+        wall2.freezeWorldMatrix()
+        const wall3 = wall1.createInstance('wall3')
+        wall3.parent = root
+        wall3.position.x = 0
+        wall3.position.z = -50
+        wall3.rotation.y = Math.PI
+        wall3.freezeWorldMatrix()
+        const wall4 = wall1.createInstance('wall4')
+        wall4.parent = root
+        wall4.position.x = -50
+        wall4.position.z = 0
+        wall4.rotation.y = 3 * Math.PI / 2
+        wall4.freezeWorldMatrix()
+
+        // Create ground
+        const ground1 = MeshBuilder.CreateGround('ground1', { height: 50, width: 50, subdivisions: 1 })
+        ground1.material = this.#matFactory.create('Metal_Plate_15', {pScale: 0.01, uScale: 20, vScale: 20})
+        ground1.parent = root
+        ground1.position = new Vector3(-25, 0, -25)
+        ground1.freezeWorldMatrix()
+        const ground2 = ground1.createInstance('ground2')
+        ground2.parent = root
+        ground2.position = new Vector3(-25, 0, 25)
+        ground2.freezeWorldMatrix()
+        const ground3 = ground1.createInstance('ground3')
+        ground3.parent = root
+        ground3.position = new Vector3(25, 0, -25)
+        ground3.freezeWorldMatrix()
+        const ground4 = ground1.createInstance('ground4')
+        ground4.parent = root
+        ground4.position = new Vector3(25, 0, 25)
+        ground4.freezeWorldMatrix()
     }
 
     #createPointLight = (position, parent) => {
