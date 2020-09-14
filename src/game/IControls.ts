0ff212f1caae36d81bb39cf24abdb8aed9e17f3f
Cyrus <csarkosh@uw.edu>
2020-09-13 23:49:22 -0700

    Update doc comments

--- src/game/IControls.ts
+++ src/game/IControls.ts
@@ -1,10 +1,19 @@
 import {Vector2, Vector3} from "@babylonjs/core";
 
 export default interface IControls {
+    /**
+     * Cleans up internal bindings
+     */
     destroy(): void
+
     /**
      * Returns the intended movement direction
      */
     direction(): Vector3
+
+    /**
+     * Returns a rotation relative to the rotation
+     * of the previous frame
+     */
     rotation(): Vector2
 }
